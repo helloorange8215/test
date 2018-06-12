0a642a8912bd6fe521844891e2d626e3d2b3c3fa
helloorange8215 <helloorange8215@gmail.com>
2018-06-11 21:00:37 -0400

    my update msg

--- refresh_urls.py
+++ refresh_urls.py
@@ -1,26 +1,32 @@
 import os, requests, subprocess
 from bs4 import BeautifulSoup
 
+
 def refresh_urls():
-  r"""
-  How to websurf less and restrict videogames on OS X
-  1. In system preferences: Create an admin account and set a for-use account as standard privileges
-  2. Restart computer
-  3. Log in as admin and remove videogame applications
-  4. In terminal: Use sudo crontab to read from a designated file in the standard account which acts as a 1-way add-only text file that lists domains to be added into /etc/hosts
-  5. Change admin password to a not-to-be-memorized password that's written on paper and locked in a timed-unlock box and log out of admin
-  """
-  whitelisted_domains = []
+  current_blacklisted_domains = open("/etc/hosts").read()
   url = "https://github.com/helloorange8215/test/blob/master/test.py"
-  domain_list = [i.text for i in BeautifulSoup(requests.get(url).text, "lxml").findAll("td", attrs={"class":"blob-code blob-code-inner js-file-line"})]
+  domain_list = [i.text for i in BeautifulSoup(requests.get(url).text).findAll("td", attrs={"class":"blob-code blob-code-inner js-file-line"})]
   for i in domain_list:
-    current_blacklisted_domains = open("/etc/hosts").read()
-    if (i not in current_blacklisted_domains) and (i not in whitelisted_domains):
-      print("domain %s not currently in /etc/hosts, adding it" % i)
-      with open("/etc/hosts", "a") as f:
-        f.write("127.0.0.1\t{}\n".format(i))
-        f.write("127.0.0.1\twww.{}\n".format(i))
+    with open("/etc/hosts","a") as f:
+      if ("127.0.0.1\t%s www.%s\n" %(i,i)) not in current_blacklisted_domains:
+        f.write("127.0.0.1\t%s www.%s\n"%(i,i))
+
+def uniq_etc_hosts():
+  os.system("sudo sort -u -o /etc/hosts /etc/hosts")
+
+def firefox_check():
+  def check_output(cmd):
+    return subprocess.check_output(cmd.split(), shell=False,stdin=None, stderr=None, close_fds=True).decode()
+  if "46" not in check_output("/Applications/Firefox.app/Contents/MacOS/firefox-bin -v"):
+    os.system("sudo rm -rf /Applications/Firefox.app")
+    os.system("sudo cp -r /Applications/Firefox_46.app /Applications/Firefox.app")
+    os.system("sudo touch /Users/paul/Notification_Recently_Reinstalled_Firefox46")
 
 
 if __name__ == "__main__":
   refresh_urls()
+  uniq_etc_hosts()
+  firefox_check()
+
+
+# Patched - 06.09.18 - Resolved all TLDS.
